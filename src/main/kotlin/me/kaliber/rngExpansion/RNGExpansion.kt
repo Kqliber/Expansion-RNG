424a7e1dac0c3a6f10894db01947ec9982d2fb12
Kqliber <Dylansaidler03@gmail.com>
2020-12-15 23:39:42 +0000

    converted to allman code style, added last generated number placeholder

--- src/main/kotlin/me/kaliber/rngExpansion/RNGExpansion.kt
+++ src/main/kotlin/me/kaliber/rngExpansion/RNGExpansion.kt
@@ -5,43 +5,68 @@ import kotlin.math.max
 import org.bukkit.OfflinePlayer
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
 
-class RNGExpansion : PlaceholderExpansion() {
+class RNGExpansion : PlaceholderExpansion()
+{
 
-    override fun canRegister(): Boolean {
+    override fun canRegister(): Boolean
+    {
         return true
     }
 
-    override fun getAuthor(): String {
+    override fun getAuthor(): String
+    {
         return "Kaliber"
     }
 
-    override fun getVersion(): String {
-        return "1.1.1"
+    override fun getVersion(): String
+    {
+        return "1.2"
     }
 
-    override fun getIdentifier(): String {
+    override fun getIdentifier(): String
+    {
         return "rng"
     }
 
-    override fun onRequest(p: OfflinePlayer, input: String): String? {
-        when {
-            input == "random" -> return (1..Int.MAX_VALUE).random().toString()
+    private var lastNumber = 0
 
-            input.toIntOrNull() != null -> {
-                return (input.toInt()..Int.MAX_VALUE).random().toString()
+    override fun onRequest(p: OfflinePlayer, input: String): String?
+    {
+       return when
+       {
+            input == "random" ->
+            {
+                val number = (1..Int.MAX_VALUE).random()
+                lastNumber = number
+                number.toString()
             }
 
-            input.contains(',') -> {
+            input == "last_generated" -> lastNumber.toString()
+
+            input.toIntOrNull() != null ->
+            {
+                val number = (input.toInt()..Int.MAX_VALUE).random()
+                lastNumber = number
+                number.toString()
+            }
+
+            input.contains(',') ->
+            {
                 val args = input.split(',')
-                if (args[0].toIntOrNull() == null || args[1].toIntOrNull() == null) return null
+                if (args[0].toIntOrNull() == null || args[1].toIntOrNull() == null)
+                {
+                    return null
+                }
                 
                 val min = min(args[0].toInt(), args[1].toInt())
                 val max = max(args[0].toInt(), args[1].toInt())
-                
-                return (min..max).random().toString()
+
+                val number = (min..max).random()
+                lastNumber = number
+                number.toString()
             }
+           else -> null
         }
-        return null
     }
 
 }
