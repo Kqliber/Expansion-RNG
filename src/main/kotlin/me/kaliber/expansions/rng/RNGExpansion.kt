040880918c0c98e00af394a39d1bbf5a11b89d00
Kqliber <Dylansaidler03@gmail.com>
2021-02-13 19:48:38 +0000

    version bump

--- src/main/kotlin/me/kaliber/expansions/rng/RNGExpansion.kt
+++ src/main/kotlin/me/kaliber/expansions/rng/RNGExpansion.kt
@@ -18,7 +18,7 @@ class RNGExpansion : PlaceholderExpansion()
 
     override fun getVersion(): String
     {
-        return "1.2"
+        return "1.3"
     }
 
     override fun getIdentifier(): String
